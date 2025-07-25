8e13cea89a032408e710fff49a5de7fddda5866c
loukawstl <louka.wasteels60@gmail.com>
2025-07-25 15:38:22 +0200

    Normalize line endings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
-// tailwind.config.js
 module.exports = {
   content: [
     "./*.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  safelist: [
+    'text-primary',
+    'border-primary',
+    'bg-primary'
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#00df9a',
+      }
+    }
   },
   plugins: [],
 }
